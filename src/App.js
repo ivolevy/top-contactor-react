f8cad6ca483327e9f39aa1096ec1b8362688e17e
Ivan Levy <ivanlevy@MacBook-Air-de-Ivan.local>
2023-11-20 16:34:32 -0300

    adding a view

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './views/Home';
 import Apply from './views/Apply';
 
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/apply" element={<Apply />} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ src/views/Apply.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const Apply = () => {
+  return (
+    <div>
+      <Header />
+      {/* header */}
+      {/* benefits */}
+      {/* list works */}
+      {/* cta  */}
+      <Footer />
+    </div>
+  );
+};
+
+export default Apply;
