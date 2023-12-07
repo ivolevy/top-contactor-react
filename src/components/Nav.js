59fe8d0b4ceb38946b44c76dfcb03b0e79cf41f5
Ivan Levy <ivanlevy@MacBook-Air-de-Ivan.local>
2023-12-07 16:36:07 -0300

    adding nav effects

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,28 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Container, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Navbar, Container, Button } from 'react-bootstrap'; // Agrega estas importaciones
+import { NavLink, useLocation } from 'react-router-dom';
 import logo from '../assets/images/logo&text.png';
-import '../assets/styles/Nav.css'
+import '../assets/styles/Nav.css';
 
 const Nav = () => {
+  const location = useLocation();
+
   return (
     <Navbar bg="rgba(255, 255, 255, 0.5)" expand="lg" className="position-absolute w-100 z-index-1">
       <Container>
         <Navbar.Brand href="#">
-          <Link to="/">
+          <NavLink to="/">
             <img src={logo} alt="" width="140" height="80" className="d-inline-block align-top img logoNav" />
-          </Link>
+          </NavLink>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-          <Link to="/">
-            <Button className="me-4 buttonNav">Find your perfect contractor</Button>
-          </Link>
-          <Link to="/Contractors">
-            <Button className="me-2 buttonNav">Apply to join as a TopContractor</Button>
-          </Link>
+          <NavLink to="/" activeClassName="active">
+            <Button className={`me-4 buttonNav ${location.pathname !== '/' && 'transparentBg'}`}>Find your perfect contractor</Button>
+          </NavLink>
+          <NavLink to="/Contractors" activeClassName="active">
+            <Button className={`me-2 buttonNav ${location.pathname !== '/Contractors' && 'transparentBg'}`}>Apply to join as a TopContractor</Button>
+          </NavLink>
         </Navbar.Collapse>
       </Container>
     </Navbar>
