1dad719e74a8e1aef1522abdcf8e1992f1ab3bd0
Ivan Levy <ivanlevy@MacBook-Air-de-Ivan.local>
2023-12-13 14:31:56 -0300

    adding front form to contractors header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './views/Home';
 import Contractors from './views/Contractors';
 import Privacy from './views/Privacy';
-import Blog from './views/Blog';
 import Thankyou from './views/Thankyou';
 
 const App = () => {
@@ -13,7 +12,6 @@ const App = () => {
         <Route exact path="/" element={<Home />} />
         <Route path="/Contractors" element={<Contractors />} />
         <Route path="/Privacy" element={<Privacy />} />
-        <Route path="/Blog" element={<Blog />} />
         <Route path="/Thank-you" element={<Thankyou />} />
       </Routes>
     </Router>
--- src/components/contractors/Header.jsx
+++ src/components/contractors/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import '../../assets/styles/Contractors.css';
 import '../../assets/styles/Styles.css';
-
-{/** APPLY HEADER */}
+import { IoTriangle } from "react-icons/io5";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -19,7 +19,18 @@ const Header = () => {
           </div>
         </div>
         <div className="headerServicesContractors">
-          <div className="hsContractors">            
+          <div className="hsContractors">
+            <div className='formDiv'>
+              <form className='form'>
+                  <input className='input' type="text" name="name" placeholder='Zip code'/>
+                  <input className='input inputBottom' type="text" name="name" placeholder='Tell us what services you offer!'/>
+              </form>
+              <div className='buttonDiv'>
+                <Link to="/Thank-you" className='buttonLink'>
+                  <button className='buttonHs'>Next <IoTriangle className='triangle'/></button>
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
