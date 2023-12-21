b67a05788115f0fd5c04d90f77817d21529d60b7
Ivan Levy <ivanlevy@MacBook-Air-de-Ivan.local>
2023-12-21 11:03:38 -0300

    privacy policiy

--- src/components/contractors/Works.jsx
+++ src/components/contractors/Works.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import '../../assets/styles/Contractors.css';
 import '../../assets/styles/Styles.css';
-import { LuListVideo } from "react-icons/lu";
-import { LiaHandshake } from "react-icons/lia";
-import { IoIosCalculator } from "react-icons/io";
+import { PiChatCenteredTextFill } from "react-icons/pi";
+import { FaLocationDot } from "react-icons/fa6";
+import { TbCoinFilled } from "react-icons/tb";
 
 const steps = [
   {
-    icon: <LuListVideo className="iconWork" />,
+    icon: <PiChatCenteredTextFill className="iconWork" />,
     title: 'STEP 1',
     text: 'Homeowners Request a Project Estimate From The "Top" Local Contractor',
     desc: 'Homeowners are pre-vetted with survey questions to ensure they are serious.',
   },
   {
-    icon: <LiaHandshake className="iconWork" />,
+    icon: <FaLocationDot className="iconWork" />,
     title: 'STEP 2',
     text: 'Homeowners are connected with local contractors',
     desc: 'We will match each homeowner with a top local contractor. Leads & booked appointments are sent to the top local contractor.',
   },
   {
-    icon: <IoIosCalculator className="iconWork" />,
+    icon: <TbCoinFilled className="iconWork" />,
     title: 'STEP 3',
     text: 'Contractors get new customers & Increase revenue',
     desc: 'Contractors provide estimates for the homeowners to generate more new customers and revenue.',
--- src/components/home/Tips.jsx
+++ src/components/home/Tips.jsx
@@ -12,24 +12,24 @@ const Tips = () => {
         <div className="sdi">
             <div className="squareDivImg"></div>
         </div>
-        <p className="squareTextTips">Jen K, Hampstead</p>
-        <p className="SquareDescTips">Every single detail and concern I had was addressed in a timely and professional manner. <span className="strong">I highly recommend them</span>.</p>
+        <p className="squareTextTips">Common Roofing Problems</p>
+        <p className="SquareDescTips">As a homeowner, you know how important a roof is to the integrity of your home. Unfortunately, roofs don't last forever and can be susceptible to a variety of problems over time. In this article, we'll</p>
       </div>
 
       <div className="squareTips">
         <div className="sdi">
             <div className="squareDivImg squareDivImg2"></div>
         </div>
-        <p className="squareTextTips">Jen K, Hampstead</p>
-        <p className="SquareDescTips">Every single detail and concern I had was addressed in a timely and professional manner. <span className="strong">I highly recommend them</span>.</p>
+        <p className="squareTextTips">DIY vs. Hiring a Professional Roofer</p>
+        <p className="SquareDescTips">Homeowners often face the dilemma of whether to undertake a roofing project themselves or hire a professional contractor. While DIY can save you money, it is not always the best option, especially</p>
       </div>
 
       <div className="squareTips">
         <div className="sdi">
             <div className="squareDivImg squareDivImg3"></div>
         </div>
-        <p className="squareTextTips">Jen K, Hampstead</p>
-        <p className="SquareDescTips">Every single detail and concern I had was addressed in a timely and professional manner. <span className="strong">I highly recommend them</span>..</p>
+        <p className="squareTextTips">The Importance of Regular Roof Maintenance</p>
+        <p className="SquareDescTips">When it comes to maintaining your home, regular roof maintenance is often overlooked. However, neglecting your roof can lead to costly repairs and even jeopardize the safety of your family. Hiring a</p>
       </div>
 
     </div>
